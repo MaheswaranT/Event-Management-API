31673099486ab01b3ef7b76e5fa21bd858df1a4a
MaheswaranT <maheshwaranm75@gmail.com>
2025-03-17 09:38:58 +0530

    Refactored settings.py to use environment variables for security

--- meetup_api/meetup_api/settings.py
+++ meetup_api/meetup_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config  # Import config function
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x*59yu2d)ho_gkotdzs4-4=!ry+8k7)m5!dxx*lwv==mn4ex@6'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,14 +84,15 @@ WSGI_APPLICATION = 'meetup_api.wsgi.application'
 #     }
 # }
 
+# MySQL Database Configuration
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'meetup_db',
-        'USER': 'root', 
-        'PASSWORD': '08122002@Bc',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
